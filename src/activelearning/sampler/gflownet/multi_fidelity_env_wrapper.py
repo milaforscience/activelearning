51ffc4d3762e53a06dfa914090a74d63707e8bdd
Alex <alexhg@e.email>
2026-04-13 16:16:13 -0400

    Create MultiFidelityGFlowNetEnvWrapperBase with helper methods

--- src/activelearning/sampler/gflownet/multi_fidelity_env_wrapper.py
+++ src/activelearning/sampler/gflownet/multi_fidelity_env_wrapper.py
@@ -1,10 +1,65 @@
+from typing import Any, List, Sequence, Tuple
+
 from gflownet.envs.base import GFlowNetEnv
 from gflownet.envs.choice import Choice
+from gflownet.envs.composite.base import CompositeBase
 from gflownet.envs.composite.setfix import SetFix
 from gflownet.envs.composite.stack import Stack
 
 
-class MultiFidelityGFlowNetEnvWrapper(SetFix):
+class MultiFidelityGFlowNetEnvWrapperBase(CompositeBase):
+    """Common base environment for all the multi-fidelity environment wrappers"""
+
+    def get_states_base_and_fidelities(
+        self, states: Sequence
+    ) -> Tuple[List[Any], List[int]]:
+        """Retrieves the states of the base env and the fidelities of a batch of
+        states.
+
+        Parameters
+        ----------
+        states : Sequence
+            A batch of states in environment format.
+
+        Returns
+        -------
+        states_base : List[Any]
+            The states of the base environment in the batch.
+        fidelities : List[int]
+            The fidelity index in each state of the batch.
+        """
+        states_base = []
+        fidelities = []
+        for state in states:
+            states_base.append(self._get_substate(state, self.idx_base_env))
+            fidelities.append(self._get_substate(state, self.idx_fidelity)[0])
+        return states_base, fidelities
+
+    def get_state_base_and_fidelity(self, state: Any = None) -> Tuple[Any, int]:
+        """Retrieves the state of the base env and the fidelity of a state.
+
+        This method constructs a dummy batch with the input state and calls
+        ``get_states_base_and_fidelities()``; then returns the first element of the
+        output.
+
+        Parameters
+        ----------
+        states : Any or ``None``
+            A state in environment format. If ``None``, ``self.state`` is used.
+
+        Returns
+        -------
+        states_base : Any
+            The state of the base environment.
+        fidelities : int
+            The fidelity index in the input state.
+        """
+        state = self._get_state(state)
+        states_base, fidelities = self.get_states_base_and_fidelities([state])
+        return states_base[0], fidelities[0]
+
+
+class MultiFidelityGFlowNetEnvWrapper(SetFix, MultiFidelityGFlowNetEnvWrapperBase):
     """Turns any GFlowNet environment into a multi-fidelity environment.
 
     The wrapper is a SetFix GFlowNet environment whose sub-environments are the
@@ -18,11 +73,15 @@ class MultiFidelityGFlowNetEnvWrapper(SetFix):
         **kwargs,
     ) -> None:
         self.env_base = env_base
-        self.fidelity_env = Choice(n_options=n_fidelities)
-        super().__init__(subenvs=tuple([self.env_base, self.fidelity_env]), **kwargs)
+        self.env_fidelity = Choice(n_options=n_fidelities)
+        self.idx_base_env = 0
+        self.idx_fidelity = 1
+        super().__init__(subenvs=tuple([self.env_base, self.env_fidelity]), **kwargs)
 
 
-class MultiFidelityGFlowNetEnvWrapperFidFirst(Stack):
+class MultiFidelityGFlowNetEnvWrapperFidFirst(
+    Stack, MultiFidelityGFlowNetEnvWrapperBase
+):
     """Turns any GFlowNet environment into a multi-fidelity environment.
 
     The wrapper is a Stack GFlowNet environment whose sub-environments are:
@@ -40,11 +99,15 @@ class MultiFidelityGFlowNetEnvWrapperFidFirst(Stack):
         **kwargs,
     ) -> None:
         self.env_base = env_base
-        self.fidelity_env = Choice(n_options=n_fidelities)
-        super().__init__(subenvs=tuple([self.fidelity_env, self.env_base]), **kwargs)
+        self.env_fidelity = Choice(n_options=n_fidelities)
+        self.idx_fidelity = 0
+        self.idx_base_env = 1
+        super().__init__(subenvs=tuple([self.env_fidelity, self.env_base]), **kwargs)
 
 
-class MultiFidelityGFlowNetEnvWrapperFidLast(Stack):
+class MultiFidelityGFlowNetEnvWrapperFidLast(
+    Stack, MultiFidelityGFlowNetEnvWrapperBase
+):
     """Turns any GFlowNet environment into a multi-fidelity environment.
 
     The wrapper is a Stack GFlowNet environment whose sub-environments are:
@@ -62,5 +125,7 @@ class MultiFidelityGFlowNetEnvWrapperFidLast(Stack):
         **kwargs,
     ) -> None:
         self.env_base = env_base
-        self.fidelity_env = Choice(n_options=n_fidelities)
-        super().__init__(subenvs=tuple([self.env_base, self.fidelity_env]), **kwargs)
+        self.env_fidelity = Choice(n_options=n_fidelities)
+        self.idx_base_env = 0
+        self.idx_fidelity = 1
+        super().__init__(subenvs=tuple([self.env_base, self.env_fidelity]), **kwargs)
--- tests/sampler/gflownet/test_multi_fidelity_env_wrapper.py
+++ tests/sampler/gflownet/test_multi_fidelity_env_wrapper.py
@@ -137,3 +137,82 @@ def test__env_wrapper_set_initializes_properly(env_base, n_fidelities, request):
     env_base = request.getfixturevalue(env_base)
     env = MultiFidelityGFlowNetEnvWrapper(env_base=env_base, n_fidelities=n_fidelities)
     assert isinstance(env, GFlowNetEnv)
+
+
+@pytest.mark.parametrize(
+    "env_base",
+    [
+        "env_grid2d",
+        "env_cube3d",
+        "env_choice",
+        "env_stack_cube_setgrids",
+        "env_stack_cube_setstacks",
+        "env_set_cubes",
+    ],
+)
+@pytest.mark.parametrize(
+    "n_fidelities",
+    [
+        1,
+        3,
+    ],
+)
+def test__get_states_base_and_fidelities_returns_expected(
+    env_base, n_fidelities, request
+):
+    env_base = request.getfixturevalue(env_base)
+    env = MultiFidelityGFlowNetEnvWrapper(env_base=env_base, n_fidelities=n_fidelities)
+
+    # Sample a batch of random states
+    n_states = 10
+    states = []
+    states_base_expected = []
+    fidelities_expected = []
+    for _ in range(n_states):
+        env.get_random_states(n_states=1)
+        states.append(env.state)
+        states_base_expected.append(env.env_base.state)
+        fidelity = env.env_fidelity.state[0]
+        assert fidelity in range(n_fidelities + 1)
+        fidelities_expected.append(fidelity)
+
+    # Retrieve base states and fidelities using env wrapper method
+    states_base, fidelities = env.get_states_base_and_fidelities(states)
+
+    # Check equality
+    for state, state_exp in zip(states_base, states_base_expected):
+        assert env.env_base.equal(state, state_exp)
+    assert fidelities == fidelities_expected
+
+
+@pytest.mark.parametrize(
+    "env_base",
+    [
+        "env_grid2d",
+        "env_cube3d",
+        "env_choice",
+        "env_stack_cube_setgrids",
+        "env_stack_cube_setstacks",
+        "env_set_cubes",
+    ],
+)
+@pytest.mark.parametrize(
+    "n_fidelities",
+    [
+        1,
+        3,
+    ],
+)
+def test__get_state_base_and_fidelity_returns_expected(env_base, n_fidelities, request):
+    env_base = request.getfixturevalue(env_base)
+    env = MultiFidelityGFlowNetEnvWrapper(env_base=env_base, n_fidelities=n_fidelities)
+
+    n_states = 10
+    for _ in range(n_states):
+        env.get_random_states(n_states=1)
+        state_base_expected = env.env_base.state
+        fidelity_expected = env.env_fidelity.state[0]
+        assert fidelity_expected in range(n_fidelities + 1)
+        state_base, fidelity = env.get_state_base_and_fidelity(env.state)
+        assert env.env_base.equal(state_base, state_base_expected)
+        assert fidelity == fidelity_expected
