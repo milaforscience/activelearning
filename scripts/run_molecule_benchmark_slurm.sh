61445af54c20c6a1a277feef5fc207735fdab796
Pedro Ferraz <pedro.ferraz@mila.quebec>
2026-09-18 21:05:17 -0400

    adjust script to run one job at a time

--- scripts/run_molecule_benchmark_slurm.sh
+++ scripts/run_molecule_benchmark_slurm.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 #SBATCH --job-name=xtb-ipea
 #SBATCH --partition=main
-#SBATCH --array=0-23%4
+#SBATCH --array=0-23%1
 #SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=8
 #SBATCH --mem=32G
